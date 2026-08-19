e8d16ce5a3112ffe2a787a9a014bbd56f210ea58
antonyakshaygi <akshayignatius47@gmail.com>
2026-08-19 22:47:06 +0530

    Update sync_api.php

--- sync_api.php
+++ sync_api.php
@@ -54,7 +54,7 @@ try {
         PDO::ATTR_ERRMODE                  => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE       => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES         => false,
-        PDO::MYSQL_ATTR_SSL_CA             => '/etc/ssl/certs/ca-certificates.crt',
+        PDO::MYSQL_ATTR_SSL_CA             => '/etc/secrets/ca.pem', // Replace with the exact Filename defined in Render
         PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
     ];
     $pdo = new PDO($dsn, $dbUser, $dbPass, $options);
